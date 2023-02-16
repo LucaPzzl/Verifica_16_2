a1efbe0be680c1323caceac2d3295462858c8861
LucaPzzl <119593629+LucaPzzl@users.noreply.github.com>
2023-02-16 13:00:31 +0100

    Add files via upload

---
+++ pyton/luca_pezzolati.py
@@ -0,0 +1,11 @@
+import main_giovanni
+import saluto_alex_foli
+
+def saluto_nome_cognome():
+    print("Salve Luca Pezzolati")
+print(saluto_nome_cognome())
+
+main.giovanni.saluto_nome_cognome()
+main.saluto_alex_foli.saluto_nome_cognome()
+
+
