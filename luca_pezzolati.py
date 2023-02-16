d29ef49bc43ca0e4f709d27eeb127ac618cbea9b
LucaPzzl <119593629+LucaPzzl@users.noreply.github.com>
2023-02-16 13:00:58 +0100

    Update luca_pezzolati.py

--- luca_pezzolati.py
+++ luca_pezzolati.py
@@ -1,3 +1,9 @@
+mport main_giovanni
+import saluto_alex_foli
+
 def saluto_nome_cognome():
     print("Salve Luca Pezzolati")
 print(saluto_nome_cognome())
+
+main.giovanni.saluto_nome_cognome()
+main.saluto_alex_foli.saluto_nome_cognome()
